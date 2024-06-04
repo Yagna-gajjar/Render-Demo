b89970b06f8d87f905936d3a49432d171b306cab
Yagna-gajjar <yagnagangajaliya@gmail.com>
2024-06-04 14:18:02 +0530

    changes made

--- Render-Demo/index.js
+++ Render-Demo/index.js
@@ -4,7 +4,7 @@ const dotenv = require("dotenv");
 const app = express();
 dotenv.config();
 const Secret = process.env.secret
-const port = process.env.port || 5000
+const port = process.env.port || 4000
 
 app.get('/', (req, res) => {
     res.status(200).send("server started");
